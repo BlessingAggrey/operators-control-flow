528ca2b08c79423c24799f541333ea90b59abb42
BlessingAggrey <dewigsupreme@outlook.com>
2023-02-19 10:23:19 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// let result = document.getElementById('result')
+
+// result.innerText =  'Result here...'
+
+// Arithmetic Operations
+let a = 100;
+let b = 45;
+
+let sum = a + b;
+console.log(sum)
