d2e8f18b8ea99dcbbbb3931745da74ff7cb5046d
BlessingAggrey <dewigsupreme@outlook.com>
2023-02-19 21:47:15 -0400

    if else done

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 // result.innerText =  'Result here...'
 
-// Arithmetic Operations
-let a = 100;
-let b = 45;
+// Program for Bolatito  in Greenfield high school
 
-let sum = a + b;
-console.log(sum)
+let group = 'Arts'
+
+if ( group === 'Arts') {console.log('Government, Economics, Literature, History');}
+else if ( group === 'Sciences') {console.log('Physics, Chemistry, Biology, Technical Drawing');}
+else if ( group === 'Social Sciences') {console.log('Accounting, Commerce, Marketing, Geography');}
+else {console.log('English, Mathematics');}
